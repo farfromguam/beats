3f0033165e4ee8db42208865c8ff1d24b7c4568e
Christopher Fryman <christopher@fortyau.com>
2017-07-10 10:31:21 -0500

    use esnext

--- build/script.js
+++ build/script.js
@@ -1,23 +1,22 @@
+"use strict";
+
+/*jshint esnext: true */
 
 /*global angular:true */
 /*global console:true */
 /*global _:true */
 
-
-angular.module("clockApp", [])
-
-.controller("clockCtrl", function($scope) {
+angular.module("clockApp", []).controller("clockCtrl", function ($scope) {
   var day_duration = 60 * 60 * 24;
   var beat_duration = day_duration / 1000;
-  $scope.Math   = Math;
+  $scope.Math = Math;
   $scope.labels = [];
-  $scope.svgs   = [];
-  $scope.pies   = [
-    { start: 300, end: 600, radius: 200, color:"#fad541"},
-    { start: 800, end: 900, radius: 200, color:"#AEB6BF"},
-    { start: 900, end: 200, radius: 200, color:"#5D6D7E"}
-  ];
-
+  $scope.svgs = [];
+  // $scope.pies   = [
+  //   { start: 300, end: 600, radius: 200, color:"#fad541"},
+  //   { start: 800, end: 900, radius: 200, color:"#AEB6BF"},
+  //   { start: 900, end: 200, radius: 200, color:"#5D6D7E"}
+  // ];
 
 
   $scope.clock = {
@@ -26,77 +25,75 @@ angular.module("clockApp", [])
         abb: "bt",
         name: "beat",
         rotate: false,
-        orientation: function() {
-          return (Math.floor($scope.beat.pulse / 20) * 2) * 3.6 + 180;
-        },
+        orientation: function orientation() {
+          return Math.floor($scope.beat.pulse / 20) * 2 * 3.6 + 180;
+        }
       },
       twenny: { // Twenty Beats / 1/50th of a day / about 30 minutes
         abb: "tw",
         name: "twenny",
         rotate: false,
-        orientation: function() {
-          return (($scope.beat.pulse % 20) * 5) * 3.6 + 180;
-        },
+        orientation: function orientation() {
+          return $scope.beat.pulse % 20 * 5 * 3.6 + 180;
+        }
       }
     }
   };
 
-
   $scope.beat = {
-    second_to_beat: function(s) {
+    second_to_beat: function second_to_beat(s) {
       return s / 86.4;
     },
-    beat_to_second: function(b) {
+    beat_to_second: function beat_to_second(b) {
       return b * 86.4;
     },
-    elapsed_seconds: function() {
+    elapsed_seconds: function elapsed_seconds() {
       var date = new Date();
-      return date.getSeconds() + (date.getMinutes() * 60) + (date.getHours() * 3600);
+      return date.getSeconds() + date.getMinutes() * 60 + date.getHours() * 3600;
     },
-    raw_value: function() {
+    raw_value: function raw_value() {
       return $scope.beat.second_to_beat($scope.beat.elapsed_seconds()).toFixed(2);
     },
-    value: function() {
+    value: function value() {
       return Math.floor($scope.beat.raw_value());
     },
-    next_whole_beat: function() {
+    next_whole_beat: function next_whole_beat() {
       return Math.ceil($scope.beat.raw_value());
     },
-    set_beat: function(beat) {
+    set_beat: function set_beat(beat) {
       $scope.beat.pulse = beat;
       $scope.$broadcast('beat', beat);
     },
-    advance_beat: function() {
+    advance_beat: function advance_beat() {
       // console.log('advance_beat');
       $scope.beat.set_beat($scope.beat.pulse + 1);
     },
-    init: function() {
+    init: function init() {
       // set initial beat
       // console.log("attention");
       $scope.beat.set_beat($scope.beat.value());
       // calculate when next beat will be
-      var time_now_in_seconds  = $scope.beat.elapsed_seconds();
+      var time_now_in_seconds = $scope.beat.elapsed_seconds();
       var next_beat_in_seconds = $scope.beat.beat_to_second($scope.beat.next_whole_beat());
       var seconds_until_next_beat = next_beat_in_seconds - time_now_in_seconds;
       // set the next beat at the correct time, then schedule the updapte
       // console.log("march in " + Math.floor(seconds_until_next_beat) );
-      setTimeout(function() {
+      setTimeout(function () {
         // console.log("march");
         $scope.beat.advance_beat();
-        setInterval(function() {
+        setInterval(function () {
           // var direction = $scope.beat.pulse % 2 == 0 ? "left" : "right"
           // console.log(direction);
           $scope.beat.advance_beat();
         }, beat_duration * 1000);
-      }, seconds_until_next_beat * 1000 );
+      }, seconds_until_next_beat * 1000);
     }
   };
 
-
   function orient_hand(hand) {
-    var hand_element = document.querySelectorAll('.' + hand.name + '-container');
-    hand_element[0].style.webkitTransform = 'rotateZ('+ hand.orientation() +'deg)';
-    hand_element[0].style.transform       = 'rotateZ('+ hand.orientation() +'deg)';
+    var hand_element = document.querySelectorAll("." + hand.name + "-container");
+    hand_element[0].style.webkitTransform = "rotateZ(" + hand.orientation() + "deg)";
+    hand_element[0].style.transform = "rotateZ(" + hand.orientation() + "deg)";
   }
 
   function orient_hands() {
@@ -104,57 +101,36 @@ angular.module("clockApp", [])
     orient_hand($scope.clock.hands.twenny);
   }
 
-
-
-
   // create 24 hour markings
-  _.each(_.range(24), function(hour) {
-    $scope.labels.push(
-      { beat: 1000 / 24 * hour, text: hour, size: 24, radius: 53 }
-    );
+  _.each(_.range(24), function (hour) {
+    $scope.labels.push({ beat: 1000 / 24 * hour, text: hour, size: 24, radius: 53 });
     return;
   });
 
-
   // Add Swiss Clock markings
-  _.each(_.range(1000), function(beat) {
+  _.each(_.range(1000), function (beat) {
     if (beat % 100 === 0) {
-      $scope.svgs.push(
-        { beat: beat, name: "images/spritemap.svg#dash", size: "5", radius: 50 }
-      );
+      $scope.svgs.push({ beat: beat, name: "images/spritemap.svg#dash", size: "5", radius: 50 });
       return;
     } else if (beat % 20 === 0) {
-      $scope.svgs.push(
-        { beat: beat, name: "images/spritemap.svg#dot", size: "1.5", radius: 50 }
-      );
+      $scope.svgs.push({ beat: beat, name: "images/spritemap.svg#dot", size: "1.5", radius: 50 });
       return;
     } else {
       return;
     }
   });
 
-
   // add work pomodoro times
-  // TODO fix: 370 displays odd...
   var pomodoros = [310, 330, 350, 370, 410, 430, 450, 470, 530, 550, 570, 590];
-  _.each(pomodoros, function(beat) {
-    $scope.labels.push(
-      { beat: beat, text: "🍅", size: 40, radius: 80 }
-    );
+  _.each(pomodoros, function (beat) {
+    $scope.labels.push({ beat: beat, text: "🍅", size: 40, radius: 80 });
   });
-
 
   // add ideal meal times
   var meals = [300, 400, 500, 600, 700];
-  _.each(meals, function(beat) {
-    $scope.labels.push(
-      { beat: beat, text: "🍴", size: 70, radius: 25 }
-    );
+  _.each(meals, function (beat) {
+    $scope.labels.push({ beat: beat, text: "🍴", size: 70, radius: 25 });
   });
-
-
-
-
 
   function init() {
     // orient_hands whenever there is a beat announced
@@ -164,13 +140,9 @@ angular.module("clockApp", [])
     });
 
     // Lay down the beats
-    setTimeout(function() {
+    setTimeout(function () {
       $scope.beat.init();
-    }, 100 );
+    }, 100);
   }
   init();
-
-
 });
-
-
--- source/script.js
+++ source/script.js
@@ -1,3 +1,4 @@
+/*jshint esnext: true */
 
 /*global angular:true */
 /*global console:true */
@@ -6,17 +7,17 @@
 
 angular.module("clockApp", [])
 
-.controller("clockCtrl", function($scope) {
-  var day_duration = 60 * 60 * 24;
-  var beat_duration = day_duration / 1000;
+.controller("clockCtrl", $scope => {
+  const day_duration = 60 * 60 * 24;
+  const beat_duration = day_duration / 1000;
   $scope.Math   = Math;
   $scope.labels = [];
   $scope.svgs   = [];
-  $scope.pies   = [
-    { start: 300, end: 600, radius: 200, color:"#fad541"},
-    { start: 800, end: 900, radius: 200, color:"#AEB6BF"},
-    { start: 900, end: 200, radius: 200, color:"#5D6D7E"}
-  ];
+  // $scope.pies   = [
+  //   { start: 300, end: 600, radius: 200, color:"#fad541"},
+  //   { start: 800, end: 900, radius: 200, color:"#AEB6BF"},
+  //   { start: 900, end: 200, radius: 200, color:"#5D6D7E"}
+  // ];
 
 
 
@@ -26,7 +27,7 @@ angular.module("clockApp", [])
         abb: "bt",
         name: "beat",
         rotate: false,
-        orientation: function() {
+        orientation() {
           return (Math.floor($scope.beat.pulse / 20) * 2) * 3.6 + 180;
         },
       },
@@ -34,7 +35,7 @@ angular.module("clockApp", [])
         abb: "tw",
         name: "twenny",
         rotate: false,
-        orientation: function() {
+        orientation() {
           return (($scope.beat.pulse % 20) * 5) * 3.6 + 180;
         },
       }
@@ -43,47 +44,48 @@ angular.module("clockApp", [])
 
 
   $scope.beat = {
-    second_to_beat: function(s) {
+
+    second_to_beat(s) {
       return s / 86.4;
     },
-    beat_to_second: function(b) {
+    beat_to_second(b) {
       return b * 86.4;
     },
-    elapsed_seconds: function() {
-      var date = new Date();
+    elapsed_seconds() {
+      const date = new Date();
       return date.getSeconds() + (date.getMinutes() * 60) + (date.getHours() * 3600);
     },
-    raw_value: function() {
+    raw_value() {
       return $scope.beat.second_to_beat($scope.beat.elapsed_seconds()).toFixed(2);
     },
-    value: function() {
+    value() {
       return Math.floor($scope.beat.raw_value());
     },
-    next_whole_beat: function() {
+    next_whole_beat() {
       return Math.ceil($scope.beat.raw_value());
     },
-    set_beat: function(beat) {
+    set_beat(beat) {
       $scope.beat.pulse = beat;
       $scope.$broadcast('beat', beat);
     },
-    advance_beat: function() {
+    advance_beat() {
       // console.log('advance_beat');
       $scope.beat.set_beat($scope.beat.pulse + 1);
     },
-    init: function() {
+    init() {
       // set initial beat
       // console.log("attention");
       $scope.beat.set_beat($scope.beat.value());
       // calculate when next beat will be
-      var time_now_in_seconds  = $scope.beat.elapsed_seconds();
-      var next_beat_in_seconds = $scope.beat.beat_to_second($scope.beat.next_whole_beat());
-      var seconds_until_next_beat = next_beat_in_seconds - time_now_in_seconds;
+      const time_now_in_seconds  = $scope.beat.elapsed_seconds();
+      const next_beat_in_seconds = $scope.beat.beat_to_second($scope.beat.next_whole_beat());
+      const seconds_until_next_beat = next_beat_in_seconds - time_now_in_seconds;
       // set the next beat at the correct time, then schedule the updapte
       // console.log("march in " + Math.floor(seconds_until_next_beat) );
-      setTimeout(function() {
+      setTimeout(() => {
         // console.log("march");
         $scope.beat.advance_beat();
-        setInterval(function() {
+        setInterval(() => {
           // var direction = $scope.beat.pulse % 2 == 0 ? "left" : "right"
           // console.log(direction);
           $scope.beat.advance_beat();
@@ -94,9 +96,9 @@ angular.module("clockApp", [])
 
 
   function orient_hand(hand) {
-    var hand_element = document.querySelectorAll('.' + hand.name + '-container');
-    hand_element[0].style.webkitTransform = 'rotateZ('+ hand.orientation() +'deg)';
-    hand_element[0].style.transform       = 'rotateZ('+ hand.orientation() +'deg)';
+    const hand_element = document.querySelectorAll(`.${hand.name}-container`);
+    hand_element[0].style.webkitTransform = `rotateZ(${hand.orientation()}deg)`;
+    hand_element[0].style.transform       = `rotateZ(${hand.orientation()}deg)`;
   }
 
   function orient_hands() {
@@ -108,7 +110,7 @@ angular.module("clockApp", [])
 
 
   // create 24 hour markings
-  _.each(_.range(24), function(hour) {
+  _.each(_.range(24), hour => {
     $scope.labels.push(
       { beat: 1000 / 24 * hour, text: hour, size: 24, radius: 53 }
     );
@@ -117,38 +119,37 @@ angular.module("clockApp", [])
 
 
   // Add Swiss Clock markings
-  _.each(_.range(1000), function(beat) {
-    if (beat % 100 === 0) {
-      $scope.svgs.push(
-        { beat: beat, name: "images/spritemap.svg#dash", size: "5", radius: 50 }
-      );
-      return;
-    } else if (beat % 20 === 0) {
-      $scope.svgs.push(
-        { beat: beat, name: "images/spritemap.svg#dot", size: "1.5", radius: 50 }
-      );
-      return;
-    } else {
-      return;
-    }
+  _.each(_.range(1000), beat => {
+     if (beat % 100 === 0) {
+       $scope.svgs.push(
+         { beat, name: "images/spritemap.svg#dash", size: "5", radius: 50 }
+       );
+       return;
+     } else if (beat % 20 === 0) {
+       $scope.svgs.push(
+         { beat, name: "images/spritemap.svg#dot", size: "1.5", radius: 50 }
+       );
+       return;
+     } else {
+       return;
+     }
   });
 
 
   // add work pomodoro times
-  // TODO fix: 370 displays odd...
-  var pomodoros = [310, 330, 350, 370, 410, 430, 450, 470, 530, 550, 570, 590];
-  _.each(pomodoros, function(beat) {
+  const pomodoros = [310, 330, 350, 370, 410, 430, 450, 470, 530, 550, 570, 590];
+  _.each(pomodoros, beat => {
     $scope.labels.push(
-      { beat: beat, text: "🍅", size: 40, radius: 80 }
+      { beat, text: "🍅", size: 40, radius: 80 }
     );
   });
 
 
   // add ideal meal times
-  var meals = [300, 400, 500, 600, 700];
-  _.each(meals, function(beat) {
+  const meals = [300, 400, 500, 600, 700];
+  _.each(meals, beat => {
     $scope.labels.push(
-      { beat: beat, text: "🍴", size: 70, radius: 25 }
+      { beat, text: "🍴", size: 70, radius: 25 }
     );
   });
 
@@ -158,13 +159,13 @@ angular.module("clockApp", [])
 
   function init() {
     // orient_hands whenever there is a beat announced
-    $scope.$on('beat', function (event, data) {
-      console.log("beat " + data);
+    $scope.$on('beat', (event, data) => {
+      console.log(`beat ${data}`);
       return orient_hands();
     });
 
     // Lay down the beats
-    setTimeout(function() {
+    setTimeout(() => {
       $scope.beat.init();
     }, 100 );
   }
